5e9c935b0c77b0f45608450780d720a76ff79247
Masa Fukui <masahiro.m.fukui@gmail.com>
2025-01-28 22:06:16 +0900

    update

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -287,12 +287,7 @@ return {
       lazy = false, -- This plugin is already lazy
     },
     {
-        'MeanderingProgrammer/render-markdown.nvim',
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    }
+        "OXY2DEV/markview.nvim",
+        lazy = false
+    },
 }
